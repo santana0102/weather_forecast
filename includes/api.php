2d7a8cdfbd60a79db6de63347a2b71ace6a8adf2
santana0102 <gusantanasgrp@gmail.com>
2022-02-10 15:32:08 -0300

    new pages

--- includes/api.php
+++ includes/api.php
@@ -1,13 +1,31 @@
 <?php
 
-    $city = "imbituba";
-    $api_key = "fd2b411102032416d92ceac21b00d7ef";
-    $url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key"
-    
-    $api_data = json_decode(file_get_contents($url));
+$city = filter_input(INPUT_POST, "city", FILTER_SANITIZE_SPECIAL_CHARS);
+$api_key = "fd2b411102032416d92ceac21b00d7ef";
+$curl = curl_init();
 
-    echo $api_data;
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid=$api_key",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "cache-control: no-cache"
+    ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+$response = json_decode($response, true);
+
+$temperature = ($response['main']['temp']) - 273.15;
+$forecasts = ($response['weather'][0]['description']);
+$humidity = ($response['main']['humidity']);
+$wind_speed = ($respense['wind']['speed']);
 
 
 
-?>
--- index.php
+++ index.php
@@ -11,29 +11,18 @@
 </head>
 
 <body>
-    <div class="center">
-        <?php
-            require("includes/api.php");
-        ?>
-        <h1>Type the city informations to get the current weather forecast.</h1>
-        <form action="/pages/loginbd.php" method="POST">
+    <div class="center">  
+    <h1>Type the city informations to get the current weather forecast.</h1>
+        <form action="includes/api.php" method="POST">
             <div class="txt_field">
                 <input type="text" name="city" id="city" required>
                 <span></span>
                 <label>City name</label>
             </div>
-            <div class="txt_field">
-                <input type="text" name="state" required>
-                <span></span>
-                <label>State code</label>
-            </div>
             <input type="submit" value="Bring me the results">
         </form>
     </div>
 </body>
-
-
-
 
 
 
